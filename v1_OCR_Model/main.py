feac09cb5016c339790a06d47ed32c97065e45f2
Trevor Cloutier <143129943+trevor-mnsu@users.noreply.github.com>
2026-02-16 14:18:19 -0600

    added empty files
    python files added for needed features

--- v1_OCR_Model/main.py
+++ v1_OCR_Model/main.py
@@ -1,14 +1,12 @@
 # Import Libraries
 import cv2
-import pytesseract
-import re
-
-from phases.phase1 import phase1
+from preprocessing.phase1 import phase1
 from search_text.search_text import search_text
 
 # Open images using OpenCV (NOT PIL)
 img1 = cv2.imread("v1_OCR_Model/images/pillblisterwhite.png")
 img2 = cv2.imread("v1_OCR_Model/images/pillblistertin.png")
+
 
 # Try phase 1 
 text = phase1(img1)
