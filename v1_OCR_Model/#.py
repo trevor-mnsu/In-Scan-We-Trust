5e34e21f29da0aba9ea6b3d48bcbbc3bbc593b6e
Trevor Cloutier <143129943+trevor-mnsu@users.noreply.github.com>
2026-02-19 15:17:37 -0600

    file path changes
    change image file path

--- v1_OCR_Model/#.py
+++ v1_OCR_Model/#.py
@@ -3,9 +3,9 @@ import cv2
 from preprocessing.apply_all_preprocessing import apply_all_preprocesses
 
 # Open images using OpenCV (NOT PIL)
-img1 = cv2.imread("v1_OCR_Model/images/pillblisterwhite.png")
-img2 = cv2.imread("v1_OCR_Model/images/pillblistertin.png")
+img1 = cv2.imread("images/pillblisterwhite.png")
+img2 = cv2.imread("images/pillblistertin.png")
 
 # Finding the best preprocessing techniques
-apply_all_preprocesses(img2)
+apply_all_preprocesses(img1)
 
