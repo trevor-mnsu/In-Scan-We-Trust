875324f3d57fa20b3bca1ce5e23fd7b09b614d18
MiReh2 <mireh2002@outlook.com>
2026-04-10 09:59:20 -0500

    Add adaptive threshold preprocessing for blister OCR

---
+++ v1_OCR_Model/preprocessing/adaptivethreshold.py
@@ -0,0 +1,14 @@
+import cv2
+
+def adaptive_threshold(img):
+   
+    thresh = cv2.adaptiveThreshold(
+        cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 
+        255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY_INV,
+        15,
+        4
+    )
+
+    return thresh
