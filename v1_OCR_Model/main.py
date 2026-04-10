bbd96724d718eb01886c019896678d9d6cdeaf71
Blake Reamer <breamer19@outlook.com>
2026-04-10 10:28:10 -0500

    Switch OCR sample image to pillblisterwhite

--- v1_OCR_Model/main.py
+++ v1_OCR_Model/main.py
@@ -12,12 +12,12 @@ base_dir = Path(__file__).resolve().parent
 images_dir = base_dir / "images"
 
 image_paths = [
-    #str(images_dir / "pillblisterwhite.png"),
+    str(images_dir / "pillblisterwhite.png"),
     #str(images_dir / "pillblistertin.png"),
     #str(images_dir / "ParcetamolTin.png"),
     #str(images_dir / "IbuprofenTin.png"),
     #str(images_dir / "ErythromycinTin.png"),
-    str(images_dir / "Atorvastatin.png"),
+    #str(images_dir / "Atorvastatin.png"),
     #str(images_dir / "atorvastatin2.png"),
     #str(images_dir / "amoxycillin2.png"),
     #str(images_dir / "amoxycillin2.png"),
